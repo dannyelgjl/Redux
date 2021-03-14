f83b374c4213c0bb0caaba015255140ca3a6c0f5
dannyelgjl <dannyelgjl@gmail.com>
2021-03-14 04:52:29 -0300

    feature: creating duck for authentication

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,22 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import Add from './Add';
 import List from './List';
 import Cart from './Cart';
+import { useSelector } from "react-redux";
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+	const { isAisAuthenticateduthen } = useSelector(state => state.auth);
+
+	return (
+		<Route {...rest} render={props => (
+			isAisAuthenticateduthen ? (<Component {...props}/>) : (<Redirect to={{pathname: '/', state: { from: props.location }}} />)
+		)} />
+	);
+};
 
 export default () => (
 	<Switch>
 		<Route path="/list" component={List} />
-		<Route path="/add" component={Add} />
+		<PrivateRoute path="/add" component={Add} />
 		<Route path="/cart" component={Cart} />
 		<Redirect path="/" to="/list" />
 	</Switch>
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import carsReducer from './ducks/cars';
 import layoutReducer from './ducks/layout';
-import cartReducer from './ducks/cart'
+import cartReducer from './ducks/cart';
+import authReducer  from './ducks/auth';
 
 export default configureStore({
   reducer: {
     cars: carsReducer,
     layout: layoutReducer,
     cart: cartReducer,
+    auth: authReducer
   }
 });
